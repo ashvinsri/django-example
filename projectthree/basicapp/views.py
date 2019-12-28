20b3fa4c1ca1ee82e2f1f6fed7e71575ae287ce8
ashvinsri <59316380+ashvinsri@users.noreply.github.com>
2019-12-29 00:55:17 +0530

    Update views.py

--- projectthree/basicapp/views.py
+++ projectthree/basicapp/views.py
@@ -93,7 +93,7 @@ def user_login(request):
             return HttpResponse("Invalid Detail supplied")
 
     else:
-        return render(request,'login.html',{})
+        return render(request,'Login.html',{})
 
 
 
